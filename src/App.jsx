8aed431fb3341c22b2d395fd45b6b917e1deb03d
Melrizzuto <melrizzuto4@gmail.com>
2025-04-11 18:57:44 +0200

    snack 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import useSwitch from "./useSwitch";
-
+import useDate from "./useDate"
 function App() {
   const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
-    <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
-    </div>
+    <>
+      <div>
+        <h2>Snack 1</h2>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+
+      <h2>Snack 2</h2>
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </div>
+    </>
+
   );
 }
 
